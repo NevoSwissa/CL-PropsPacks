5a0b7fbd81944183e1b1e3f015e6ec4efcaa64d3
Nevo Swissa <96447671+NevoSwissa@users.noreply.github.com>
2023-06-15 14:15:20 +0300

    Add files via upload

---
+++ CL-PropsPacks/CL-PropPack2/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+lua54 'yes'
+author "NevoSwissa#0111"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/pineapplebytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pineappleaytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/jackfruitytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/jackfruitaytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/papayabytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/papayaaytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sinkistbytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sunskistaytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dragunfruitbytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dragonfruita.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/nectarinesaytp.ytyp'
---
+++ CL-PropsPacks/CL-PropsPack3/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+lua54 'yes'
+author "NevoSwissa#0111"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/breadroll.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brioche2ytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cakeytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cerealytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cheesewedgeytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/cheesewheelytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/decorativegourdytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/loafbreadytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/morange.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/potatoytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/redpepperytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/russetpotatoytp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/subsandwichytp.ytyp'
